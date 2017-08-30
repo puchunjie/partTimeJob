4b3e685428051bb474181c57f344e93cebbda8af
puchunjie <puchunjie@live.com>
2017-08-30 10:06:32 +0800

    first commit

---
+++ js/base.js
@@ -0,0 +1,34 @@
+function initializePage(){
+  bindOnResize();
+  bindToggleOn();
+  //
+  loadAgent();
+}
+
+/*---------------method---------------*/
+//
+function bindOnResize(){
+  var resize=function(){
+    var height=$(window).height();
+    $('.full-height').height(height);
+  }
+  resize();
+  $(window).resize(function(event) {
+    resize();
+  });
+}
+function bindToggleOn(){
+  $('toggle-on').click(function(event) {
+    $(this).toggleClass('on');
+  });
+}
+function loadAgent(){
+  try{
+    var $agent=$('<script type="text/javascript" src="js/agent/agent.js"></script>');
+    $agent.appendTo('body');
+
+    console.info('w3cutagent loaded.')
+  }catch(e){
+
+  }
+}
